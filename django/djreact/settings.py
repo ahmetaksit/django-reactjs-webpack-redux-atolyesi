8bc8b0a30252cfa7e4e09b3c7b21bc82f92351e0
Ahmet AKŞİT <ahmetaksit@gmail.com>
2017-09-29 15:23:18 +0300

    Django views add...
    base.html, View1.html and  Vİew2.html
    http://getbootstrap.com/ include...

--- django/djreact/settings.py
+++ django/djreact/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djreact.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'djreact/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
