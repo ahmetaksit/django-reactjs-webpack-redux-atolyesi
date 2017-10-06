f67149f8a43c73b3e584236b3125d24e664faf2a
Ahmet AKŞİT <ahmetaksit@gmail.com>
2017-10-06 15:07:15 +0300

    React class'nın geri dönüşü mesajı değiştirildi.
    Something New! => Ahmet AKŞİT tarafından değiştirildi.!

--- django/reactjs/containers/App1Container.jsx
+++ django/reactjs/containers/App1Container.jsx
@@ -8,7 +8,7 @@ export default class App1Container extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <Headline>Something New!</Headline>
+            <Headline>Ahmet AKŞİT tarafından değiştirildi.!</Headline>
           </div>
         </div>
       </div>
