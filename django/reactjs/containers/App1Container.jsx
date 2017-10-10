9223514a6f59a2cbca743da6a9e853407491f226
Ahmet AKŞİT <ahmetaksit@gmail.com>
2017-10-10 09:23:47 +0300

    Add Redux... (state'ler bir store da tutmaya yarıyor)
    Her click'lendiğinde sayı bir artıyor. Bu örneği bir de azalan şeklinde yapmakla gerekiyor.

--- django/reactjs/containers/App1Container.jsx
+++ django/reactjs/containers/App1Container.jsx
@@ -1,14 +1,29 @@
 import React from "react"
 
+import { connect } from "react-redux"
+
+import * as counterActions from "../actions/counterActions"
 import Headline from "../components/Headline"
 
+@connect(state => ({
+  counters: state.counters,
+}))
 export default class App1Container extends React.Component {
+  handleClick() {
+    let {dispatch} = this.props;
+    dispatch(counterActions.increaseCounter())
+  }
+
   render() {
+    let {counters} = this.props
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
             <Headline>Ahmet AKŞİT tarafından stage olarak değiştirildi.!</Headline>
+            <div onClick={() => this.handleClick()}>INCREASE</div>
+            <p>{counters.clicks}</p>
+            <p>{process.env.BASE_API_URL}</p>
           </div>
         </div>
       </div>
